6dfda03b2bf1ad94bfbe851d267176e386af607a
ashtishad <ashefhabib@outlook.com>
2021-10-25 08:13:23 +0600

    Leetcode: 48. Rotate Image
    Time: O(N square) Space: O(1)
    
    Algorithm:
    -----------
    1. transpose matrix
    2. reverse each row of the matrix.
    
    link:
    
    https://leetcode.com/problems/rotate-image/
    
    https://leetcode.com/problems/rotate-image/discuss/1539294/My-Solution-in-go-with-explanation-100-faster-than-other-go-submission

--- matrix/rotate-image-48/solution-48.go
+++ matrix/rotate-image-48/solution-48.go
@@ -18,25 +18,33 @@ func rotate(matrix [][]int) {
 	if n == 1 {
 		return
 	}
+	/* rotate clock-wise = 1. transpose matrix => 2. reverse(matrix[i])
+
+	1   2  3  4      1   5  9  13        13  9  5  1
+	5   6  7  8  =>  2   6  10 14  =>    14  10 6  2
+	9  10 11 12      3   7  11 15        15  11 7  3
+	13 14 15 16      4   8  12 16        16  12 8  4
+
+	*/
 
 	for i := 0; i < n; i++ {
-		// transpose matrix
+		// transpose, i=rows, j=columns
 		for j := i + 1; j < n; j++ {
 			tmp := matrix[i][j]
 			matrix[i][j] = matrix[j][i]
 			matrix[j][i] = tmp
 		}
-		// reverse each row of the matrix
+		// reverse each row of the image
 		matrix[i] = reverse(matrix[i])
 	}
 }
 
 func main() {
 	var matrix = [][]int{
-		{5, 1, 9, 11},
-		{2, 4, 8, 10},
-		{13, 3, 6, 7},
-		{15, 14, 12, 16},
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+		{13, 14, 15, 16},
 	}
 	rotate(matrix)
 	fmt.Println(matrix)
--- templates/matrix/multi-dimensional-slices.go
+++ templates/matrix/multi-dimensional-slices.go
@@ -6,10 +6,10 @@ func main() {
 
 	// Creating multi-dimensional slice
 	s1 := [][]int{
-		{1, 2},
-		{3, 4},
-		{5, 6},
-		{7, 8},
+		{1, 2, 12},
+		{3, 4, 34},
+		{5, 6, 56},
+		{7, 8, 78},
 	}
 
 	// Accessing multi-dimensional slice
@@ -25,8 +25,8 @@ func main() {
 	// Accessing multi-dimensional slice
 	fmt.Println("Slice 2 : ", s2)
 
-	// Printing every slice inside s2
-	fmt.Println("MultiDimensional Slice s2:")
+	// Printing every slice inside s1
+	fmt.Println("MultiDimensional Slice s1:")
 	for i := 0; i < len(s1); i++ {
 		fmt.Println(s1[i])
 	}
@@ -35,13 +35,15 @@ func main() {
 	fmt.Println("Slice s2 Like Matrix:")
 
 	// number of rows in s2
-	n := len(s2)
+	n := len(s1)
+	fmt.Println("number of rows of s1 : ", n)
 
 	// number of columns in s2
-	m := len(s2[0])
+	m := len(s1[0])
+	fmt.Println("number of columns of s1 : ", m)
 	for i := 0; i < n; i++ {
 		for j := 0; j < m; j++ {
-			fmt.Print(s2[i][j] + " ")
+			fmt.Print(s1[i][j], " ")
 		}
 		fmt.Print("\n")
 	}
