30fda0ccdbbff0dfebbaa615b6b2aa9fae333f25
Ashef Habib Tishad <ashefhabib@outlook.com>
2021-10-30 20:20:07 +0600

    Leetcode 23. Merge k Sorted Lists
    Algorithm:
    ----------
    
    1.push k listnodes into h,if not nil.
     2. pop min listnode,update ptr and traverse while next is not nil
     3. return head aka sentinel.next
    
    LINK:
    
    https://leetcode.com/problems/merge-k-sorted-lists/

---
+++ ds/heap/min-heap-linked-list.go
@@ -0,0 +1,44 @@
+package ds
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// An ListNodeHeap is a min-heap of list nodes.
+type ListNodeHeap []*ListNode
+
+func (h ListNodeHeap) Len() int           { return len(h) }
+func (h ListNodeHeap) Less(i, j int) bool { return h[i].Val < h[j].Val }
+func (h ListNodeHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *ListNodeHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(*ListNode))
+}
+
+func (h *ListNodeHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// This example inserts several ints into an ListNodeHeap, checks the minimum,
+// and removes them in order of priority.
+func main() {
+	h := &ListNodeHeap{}
+	heap.Init(h)
+	heap.Push(h, 3)
+	fmt.Printf("minimum: %d\n", (*h)[0])
+	for h.Len() > 0 {
+		fmt.Printf("%d ", heap.Pop(h))
+	}
+}
