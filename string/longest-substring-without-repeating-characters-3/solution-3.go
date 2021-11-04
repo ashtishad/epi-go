2b3afb07da70c763d3ed1e4a537efd3548302c0a
Ashef Habib Tishad <ashefhabib@outlook.com>
2021-11-04 20:33:51 +0600

    3. Longest Substring Without Repeating Characters.
    Links:
    1. Explanation
    https://leetcode.com/problems/longest-substring-without-repeating-characters/discuss/1559279/Go-Submission-with-Explanation
    
    2. About ASCI Unicode UTF-8
    
    https://www.joelonsoftware.com/2003/10/08/the-absolute-minimum-every-software-developer-absolutely-positively-must-know-about-unicode-and-character-sets-no-excuses/
    
    2.

---
+++ string/longest-substring-without-repeating-characters-3/solution-3.go
@@ -0,0 +1,29 @@
+package main
+
+// abcabcbb -> 3 (abc)
+// bbbbbbb  -> 1 (b)
+
+func lengthOfLongestSubstring(s string) int {
+	var n = len(s)
+	if n <= 1 {
+		return n
+	}
+	var lp, rp, res = 0, 0, 1
+	var seen = map[byte]int{}
+	for rp < n {
+		if idx, ok := seen[s[rp]]; ok && idx >= lp {
+			lp = idx + 1
+		}
+		seen[s[rp]] = rp
+		rp++
+		res = max(res, rp-lp)
+	}
+	return res
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
