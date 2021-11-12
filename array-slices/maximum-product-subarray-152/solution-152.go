1dc8fe46da44d50debc359addcb23add322e634e
Ashef Habib Tishad <ashefhabib@outlook.com>
2021-11-12 08:56:43 +0600

    Update 152. Maximum Product Subarray

--- array-slices/maximum-product-subarray-152/solution-152.go
+++ array-slices/maximum-product-subarray-152/solution-152.go
@@ -1,8 +1,27 @@
 // https://leetcode.com/problems/maximum-product-subarray/
+// O(n) time, O(n) space
 
 package leetcode
 
-import "fmt"
+// [-2,0,-1]
+func maxProduct(nums []int) int {
+	currMin, currMax, res := nums[0], nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		// multiplied by a negative makes big number smaller, small number bigger
+		// so we redefine the extremums by swapping them
+		if nums[i] < 0 {
+			currMax, currMin = currMin, currMax
+		}
+		// max/min product for the current number is either the current number itself
+		// or the max/min by the previous number times the current one
+		currMax = max(nums[i], currMax*nums[i])
+		currMin = min(nums[i], currMin*nums[i])
+
+		// the newly computed max value is a candidate for our global result
+		res = max(res, currMax)
+	}
+	return res
+}
 
 func max(a int, b int) int {
 	if a > b {
@@ -16,23 +35,4 @@ func min(a int, b int) int {
 		return b
 	}
 	return a
-}
-
-// [-2,0,-1]
-func maxProduct(nums []int) int {
-	currMin, currMax, res := nums[0], nums[0], nums[0]
-	for i := 1; i < len(nums); i++ {
-		if nums[i] < 0 {
-			currMax, currMin = currMin, currMax
-		}
-		currMax = max(nums[i], currMax*nums[i])
-		currMin = min(nums[i], currMin*nums[i])
-		res = max(res, currMax)
-	}
-	return res
-}
-
-func main() {
-	in := []int{-2, 0, -1}
-	fmt.Println(maxProduct(in))
 }
