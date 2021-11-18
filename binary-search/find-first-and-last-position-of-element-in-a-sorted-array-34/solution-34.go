ea011de51db9069ccdc19ad508f54cb263b5e3cc
Ashef Habib Tishad <ashefhabib@outlook.com>
2021-11-18 21:10:14 +0600

    34. Find First and Last Position of Element in Sorted Array
    /*
    If A[mid] < target, then the range must begins on the right of mid (hence i = mid+1 for the next iteration)
    If A[mid] > target, it means the range must begins on the left of mid (j = mid-1)
    If A[mid] = target, then the range must begins on the left of or at mid (j= mid)
    Since we would move the search range to the same side for case 2 and 3, we might as well merge them as one single case so that less code is needed:
    */

--- binary-search/find-first-and-last-position-of-element-in-a-sorted-array-34/solution-34.go
+++ binary-search/find-first-and-last-position-of-element-in-a-sorted-array-34/solution-34.go
@@ -1,60 +1,71 @@
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 // Time: O(logN)  Space: O(1)
-// 98.7% faster than other go submissions
+
+/*
+If A[mid] < target, then the range must begins on the right of mid (hence i = mid+1 for the next iteration)
+If A[mid] > target, it means the range must begins on the left of mid (j = mid-1)
+If A[mid] = target, then the range must begins on the left of or at mid (j= mid)
+Since we would move the search range to the same side for case 2 and 3, we might as well merge them as one single case so that less code is needed:
+*/
 
 package leetcode
 
 func searchRange(nums []int, target int) []int {
-	result := []int{-1, -1}
+	var res = []int{-1, -1}
 	if len(nums) == 0 {
-		return result
+		return res
 	}
-	lo, hi := 0, len(nums)-1
-	result[0] = findStartingIndex(nums, target, lo, hi)
-	if result[0] == -1 {
-		return result
+	var lp, rp = 0, len(nums) - 1
+	res[0] = findStartingIndex(nums, target, lp, rp)
+	if res[0] == -1 {
+		return res
 	}
-	if result[0] == hi {
-		result[1] = hi
-		return result
-	}
-	result[1] = findEndingIndex(nums, target, result[0], hi)
 
-	return result
+	res[1] = findEndingIndex(nums, target, res[0], rp)
+
+	return res
 }
 
-func findStartingIndex(nums []int, target int, lo int, hi int) (index int) {
-	index = -1
-	for lo <= hi {
-		mid := lo + (hi-lo)/2
-		if nums[mid] >= target {
-			hi = mid - 1
+// Input: nums = [5,7,7,8,8,8,8,9,10], target = 8 , Output: 3
+func findStartingIndex(nums []int, target int, lp int, rp int) (idx int) {
+	idx = -1
+	for lp <= rp {
+		var mid = lp + (rp-lp)/2
+		var midVal = nums[mid]
+		// case 1: mid val is larger or equal to target
+		// case else : mid val is lower than target
+		if midVal >= target {
+			rp = mid - 1
 		} else {
-			lo = mid + 1
+			lp = mid + 1
 		}
 
-		// save the lowest target index
-		if nums[mid] == target {
-			index = mid
+		// keep updating target, we will end up with the lowest
+		if midVal == target {
+			idx = mid
 		}
 	}
-	return index
+	return idx
 }
 
-// Input: nums = [5,7,7,8,8,8,8,9,10], target = 8 , Output: [3,6]
-func findEndingIndex(nums []int, target int, lo int, hi int) (index int) {
-	index = -1
-	for lo <= hi {
-		mid := lo + (hi-lo)/2
-		if nums[mid] <= target {
-			lo = mid + 1
+// Input: nums = [5,7,7,8,8,8,8,9,10], target = 8 , Output: 6
+func findEndingIndex(nums []int, target int, lp int, rp int) (idx int) {
+	idx = -1
+	for lp <= rp {
+		var mid = lp + (rp-lp)/2
+		var midVal = nums[mid]
+		// case 1: mid val is lower or equal to target
+		// case else : mid val is higher than target
+		if midVal <= target {
+			lp = mid + 1
 		} else {
-			hi = mid - 1
+			rp = mid - 1
 		}
-		// save the highest target index
-		if nums[mid] == target {
-			index = mid
+
+		// keep updating target, we will end up with the highest
+		if midVal == target {
+			idx = mid
 		}
 	}
-	return index
+	return idx
 }
--- binary-search/find-first-and-last-position-of-element-in-a-sorted-array-34/solution-34_test.go
+++ binary-search/find-first-and-last-position-of-element-in-a-sorted-array-34/solution-34_test.go
@@ -1,43 +1,29 @@
 package leetcode
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 )
 
-type question34 struct {
-	para34
-	ans34
-}
-
-type para34 struct {
-	nums   []int
-	target int
-}
-
-type ans34 struct {
-	one []int
-}
-
-func Test_Problem34(t *testing.T) {
-
-	qs := []question34{
-
-		{
-			para34{[]int{5, 7, 7, 8, 8, 10}, 8},
-			ans34{[]int{3, 4}},
-		},
-
-		{
-			para34{[]int{5, 7, 7, 8, 8, 10}, 6},
-			ans34{[]int{-1, -1}},
-		},
+func Test_(t *testing.T) {
+	testCases := []struct {
+		nums   []int
+		target int
+		res    []int
+	}{
+		{[]int{5, 7, 7, 8, 8, 10}, 6, []int{-1, -1}},
+		{[]int{5, 7, 7, 8, 8, 10}, 8, []int{3, 4}},
 	}
 
-	fmt.Printf("------------------------Leetcode Problem 34------------------------\n")
-	for _, q := range qs {
-		_, p := q.ans34, q.para34
-		fmt.Printf("【input】:%v       【output】:%v\n", p, searchRange(p.nums, p.target))
+	for _, tc := range testCases {
+		var want = tc.res
+		got := searchRange(tc.nums, tc.target)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Source: %d\n Expected: %d\n   Actual: %d\n",
+				tc.nums,
+				want,
+				got)
+		}
 	}
-	fmt.Printf("\n\n\n")
 }
