b9d889e75bf94152b379c0813aefba741c7721a3
ashtishad <ashefhtishad@gmail.com>
2022-02-22 15:26:24 +0600

    Buy and Sell stock once. Two pointer, slow and fast.

--- epi-book/arrays/buy_and_sell_stock_once/main.go
+++ epi-book/arrays/buy_and_sell_stock_once/main.go
@@ -1,22 +1,23 @@
 package epi
 
 // Prices : []int{310, 315, 275, 295, (260), 270, (290), 230, 255, 250} Max profit: 30
+// Two pointers, slow and fast.
 func maxProfit(p []int) int {
 	res := 0
 	if len(p) == 1 {
 		return res
 	}
-	lp, rp := 0, 1
+	sp, fp := 0, 1
 
-	for rp < len(p) {
-		if p[lp] < p[rp] {
-			res = max(res, p[rp]-p[lp])
-			rp++
+	for fp < len(p) {
+		if p[sp] < p[fp] {
+			res = max(res, p[fp]-p[sp])
+			fp++
 			continue
 		}
 
-		lp = rp
-		rp++
+		sp = fp
+		fp++
 
 	}
 	return res
